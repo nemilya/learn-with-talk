a38dd63c371f683b88a05155f66823d60438270c
nemilya <nemilya@gmail.com>
2021-06-25 02:07:30 +0500

    added pronunciation parsing /.../ in word field

--- models/word.rb
+++ models/word.rb
@@ -1,3 +1,12 @@
 class Word < Sequel::Model
   many_to_one :content
+
+  def word_and_pronunciation
+    word_and_prunonciation = self.word.split(' /')
+    if word_and_pronunciation && word_and_pronunciation.size == 2
+      return word_and_pronunciation[0], '/'+word_and_pronunciation[1]
+    end
+    return self.word, nil
+  end
+
 end
