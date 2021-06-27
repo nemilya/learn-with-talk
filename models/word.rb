96e1dde6b225117feacec6d3e367d307409ebaab
nemilya <nemilya@gmail.com>
2021-06-27 19:18:21 +0500

    small fix and added position number of word

--- models/word.rb
+++ models/word.rb
@@ -2,9 +2,9 @@ class Word < Sequel::Model
   many_to_one :content
 
   def word_and_pronunciation
-    word_and_prunonciation = self.word.split(' /')
-    if word_and_pronunciation && word_and_pronunciation.size == 2
-      return word_and_pronunciation[0], '/'+word_and_pronunciation[1]
+    _word_and_pronunciation = self.word.split(' /')
+    if _word_and_pronunciation && _word_and_pronunciation.size == 2
+      return _word_and_pronunciation[0], '/'+_word_and_pronunciation[1]
     end
     return self.word, nil
   end
